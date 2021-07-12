51e091d1a80740ac16fa8c10da95404af8478eef
Agrania <arapriskaz@gmail.com>
2021-07-12 14:20:37 +0700

    fintech

---
+++ app/src/main/java/io/github/agrania/fintechresponsi/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package io.github.agrania.fintechresponsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
